02936729fcc7d886673bf6c8787b585914d93ef8
Martin Ling <martin-git@earth.li>
2022-12-14 03:21:48 +0000

    Add UI replay infrastructure and initial test case.

--- src/main.rs
+++ src/main.rs
@@ -26,10 +26,6 @@ use gtk::{
     Application,
     ApplicationWindow,
     Button,
-    MessageDialog,
-    DialogFlags,
-    MessageType,
-    ButtonsType,
     ListItem,
     ListView,
     ProgressBar,
@@ -37,6 +33,14 @@ use gtk::{
     SignalListItemFactory,
     SingleSelection,
     Orientation,
+};
+
+#[cfg(not(test))]
+use gtk::{
+    MessageDialog,
+    DialogFlags,
+    MessageType,
+    ButtonsType,
 };
 
 use pcap_file::{PcapError, pcap::{PcapReader, PcapHeader, PacketHeader}};
@@ -86,14 +90,14 @@ struct UserInterface {
     open_button: Button,
     capture_button: Button,
     stop_button: Button,
-    #[cfg(feature="record-ui-test")]
+    #[cfg(any(test, feature="record-ui-test"))]
     recording: Rc<RefCell<Recording>>,
 }
 
-#[cfg(feature="record-ui-test")]
+#[cfg(any(test, feature="record-ui-test"))]
 mod record_ui;
 
-#[cfg(feature="record-ui-test")]
+#[cfg(any(test, feature="record-ui-test"))]
 use {
     std::rc::Rc,
     crate::record_ui::Recording,
@@ -109,7 +113,7 @@ thread_local!(
 
 fn create_view<Item: 'static, Model, RowData>(
         capture: &Arc<Mutex<Capture>>,
-        #[cfg(feature="record-ui-test")]
+        #[cfg(any(test, feature="record-ui-test"))]
         recording_args: (&Rc<RefCell<Recording>>, &'static str))
     -> (Model, ListView)
     where
@@ -119,7 +123,7 @@ fn create_view<Item: 'static, Model, RowData>(
         Capture: ItemSource<Item>,
         Object: ToGenericRowData<Item>
 {
-    #[cfg(feature="record-ui-test")]
+    #[cfg(any(test, feature="record-ui-test"))]
     let (name, expand_rec, changed_rec) = {
         let (recording, name) = recording_args;
         (name, recording.clone(), recording.clone())
@@ -162,12 +166,12 @@ fn create_view<Item: 'static, Model, RowData>(
                 let model = bind_model.clone();
                 let node_ref = node_ref.clone();
                 let list_item = list_item.clone();
-                #[cfg(feature="record-ui-test")]
+                #[cfg(any(test, feature="record-ui-test"))]
                 let recording = expand_rec.clone();
                 let handler = expander.connect_expanded_notify(move |expander| {
                     let position = list_item.position();
                     let expanded = expander.is_expanded();
-                    #[cfg(feature="record-ui-test")]
+                    #[cfg(any(test, feature="record-ui-test"))]
                     recording.borrow_mut().log_item_expanded(
                         name, position, expanded);
                     display_error(
@@ -214,7 +218,7 @@ fn create_view<Item: 'static, Model, RowData>(
 
     let view = ListView::new(Some(&selection_model), Some(&factory));
 
-    #[cfg(feature="record-ui-test")]
+    #[cfg(any(test, feature="record-ui-test"))]
     model.connect_items_changed(move |model, position, removed, added|
         changed_rec.borrow_mut().log_items_changed(
             name, model, position, removed, added));
@@ -266,6 +270,7 @@ fn activate(application: &Application) -> Result<(), PacketryError> {
     header_bar.pack_start(&stop_button);
 
     window.set_titlebar(Some(&header_bar));
+    #[cfg(not(test))]
     window.show();
     WINDOW.with(|win_opt| win_opt.replace(Some(window.clone())));
 
@@ -315,7 +320,7 @@ fn activate(application: &Application) -> Result<(), PacketryError> {
     UI.with(|cell| {
         cell.borrow_mut().replace(
             UserInterface {
-                #[cfg(feature="record-ui-test")]
+                #[cfg(any(test, feature="record-ui-test"))]
                 recording: Rc::new(RefCell::new(
                     Recording::new(capture.clone()))),
                 capture,
@@ -368,13 +373,13 @@ fn reset_capture() -> Result<(), PacketryError> {
         let (traffic_model, traffic_view) =
             create_view::<TrafficItem, TrafficModel, TrafficRowData>(
                 &capture,
-                #[cfg(feature="record-ui-test")]
+                #[cfg(any(test, feature="record-ui-test"))]
                 (&ui.recording, "traffic")
             );
         let (device_model, device_view) =
             create_view::<DeviceItem, DeviceModel, DeviceRowData>(
                 &capture,
-                #[cfg(feature="record-ui-test")]
+                #[cfg(any(test, feature="record-ui-test"))]
                 (&ui.recording, "devices")
             );
         ui.capture = capture;
@@ -599,6 +604,7 @@ fn stop_luna() -> Result<(), PacketryError> {
 }
 
 fn display_error(result: Result<(), PacketryError>) {
+    #[cfg(not(test))]
     if let Err(e) = result {
         let message = format!("{}", e);
         gtk::glib::idle_add_once(move || {
@@ -623,6 +629,8 @@ fn display_error(result: Result<(), PacketryError>) {
             });
         });
     }
+    #[cfg(test)]
+    result.unwrap();
 }
 
 trait OrBug<T> {
@@ -649,4 +657,185 @@ fn main() {
     application.connect_activate(|app| display_error(activate(app)));
     application.run_with_args::<&str>(&[]);
     display_error(stop_luna());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::BufRead;
+    use itertools::assert_equal;
+    use serde_json::Deserializer;
+    use crate::record_ui::UiAction;
+
+    #[test]
+    fn test_ui_replay() {
+        let application = gtk::Application::new(
+            Some("com.greatscottgadgets.packetry.test"),
+            Default::default(),
+        );
+        application.connect_activate(|app| {
+            activate(app)
+                .expect("Failed to activate UI");
+            check_replays();
+            app.quit();
+        });
+        application.run_with_args::<&str>(&[]);
+    }
+
+    fn check_replays() {
+        let test_dir = PathBuf::from("./tests/ui/");
+        let mut list_path = test_dir.clone();
+        list_path.push("tests.txt");
+        let list_file = File::open(list_path)
+            .expect("Failed to open list of replay tests");
+        let mut comparisons = Vec::new();
+        for result in BufReader::new(list_file).lines() {
+            let test_name = result
+                .expect("Failed to read next replay test from file");
+            let mut test_path = test_dir.clone();
+            test_path.push(test_name);
+            let mut act_path = test_path.clone();
+            let mut ref_path = test_path.clone();
+            let mut out_path = test_path.clone();
+            act_path.push("actions.json");
+            ref_path.push("reference.txt");
+            out_path.push("output.txt");
+            let action_file = File::open(act_path)
+                .expect("Failed to open replay test action file");
+            let output_file = File::options()
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(out_path.clone())
+                .expect("Failed to open output file for writing");
+            with_ui(|ui| {
+                ui.recording
+                    .borrow_mut()
+                    .set_output(output_file);
+                Ok(())
+            }).unwrap();
+            comparisons.push((ref_path, out_path));
+            let mut action_reader = BufReader::new(action_file);
+            let deserializer = Deserializer::from_reader(&mut action_reader);
+            let mut replay = None;
+            for result in deserializer.into_iter::<UiAction>() {
+                use UiAction::*;
+                let action = result
+                    .expect("Failed to deserialize action");
+                match (action, &mut replay) {
+                    (Open(path), _) => {
+                        reset_capture()
+                            .expect("Resetting capture failed");
+                        let mut capture = None;
+                        with_ui(|ui| {
+                            capture = Some(ui.capture.clone());
+                            ui.recording
+                                .borrow_mut()
+                                .log_open_file(&path, &ui.capture);
+                            Ok(())
+                        }).unwrap();
+                        if let Some(capture) = capture {
+                            let file = File::open(path)
+                                .expect("Failed to open pcap file");
+                            let reader = BufReader::new(file);
+                            let pcap = PcapReader::new(reader)
+                                .expect("Failed to read pcap file");
+                            let decoder = Decoder::new();
+                            replay = Some((pcap, decoder, capture));
+                        }
+                    },
+                    (Update(count),
+                     Some((pcap, decoder, capture))) => {
+                        with_ui(|ui| {
+                            ui.recording
+                                .borrow_mut()
+                                .log_update(count);
+                            Ok(())
+                        }).unwrap();
+                        let mut cap = capture
+                            .lock()
+                            .expect("Failed to lock capture");
+                        while cap.packet_index.len() < count {
+                            let packet = pcap
+                                .next()
+                                .expect("No next pcap packet")
+                                .expect("Error in pcap reader");
+                            decoder
+                                .handle_raw_packet(&mut cap, &packet.data)
+                                .expect("Failed to decode packet");
+                        }
+                        drop(cap);
+                        update_view()
+                            .expect("Failed to update view");
+                    },
+                    (SetExpanded(name, position, expanded),
+                     Some(..)) => {
+                        with_ui(|ui| {
+                            ui.recording
+                                .borrow_mut()
+                                .log_item_expanded(
+                                    &name, position, expanded);
+                            ui.set_expanded(&name, position, expanded);
+                            Ok(())
+                        }).unwrap();
+                    },
+                    (..) => panic!("Unsupported action")
+                }
+            }
+        }
+        for (ref_path, out_path) in comparisons {
+            let ref_file = File::open(ref_path)
+                .expect("Failed to open reference file");
+            let out_file = File::open(out_path)
+                .expect("Failed to open output file for reading");
+            let ref_reader = BufReader::new(ref_file);
+            let out_reader = BufReader::new(out_file);
+            assert_equal(
+                ref_reader
+                    .lines()
+                    .map(|result| result.expect("Failed to read line")),
+                out_reader
+                    .lines()
+                    .map(|result| result.expect("Failed to read line"))
+            );
+        }
+    }
+
+    impl UserInterface {
+        fn set_expanded(&self,
+                        name: &str,
+                        position: u32,
+                        expanded: bool)
+        {
+            match name {
+                "traffic" => {
+                    let model = self.traffic_model
+                        .as_ref()
+                        .expect("UI has no traffic model");
+                    let node = model.item(position)
+                        .expect("Failed to retrieve list item")
+                        .downcast::<TrafficRowData>()
+                        .expect("List item is not TrafficRowData")
+                        .node()
+                        .expect("Failed to get node from TrafficRowData");
+                    model.set_expanded(&node, position, expanded)
+                        .expect("Failed to expand/collapse item");
+                },
+                "devices" => {
+                    let model = self.device_model
+                        .as_ref()
+                        .expect("UI has no device model");
+                    let node = model.item(position)
+                        .expect("Failed to retrieve list item")
+                        .downcast::<DeviceRowData>()
+                        .expect("List item is not DeviceRowData")
+                        .node()
+                        .expect("Failed to get node from DeviceRowData");
+                    model.set_expanded(&node, position, expanded)
+                        .expect("Failed to expand/collapse item");
+                },
+                _ => panic!("Unknown model name")
+            }
+        }
+    }
 }
