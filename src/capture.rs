2555b1d92026b13397882d9f04b9f37777c13c99
Martin Ling <martin-git@earth.li>
2022-12-17 16:22:26 +0000

    Interleave the transactions of transfers.
    This commit enables the interleaved view, and updates test cases to
    reflect the expected change in the ordering of items.

--- src/capture.rs
+++ src/capture.rs
@@ -1214,9 +1214,10 @@ impl ItemSource<TrafficItem> for CaptureReader {
                 let transaction_count = self.transfer_range(&entry)?.len();
                 let ep_traf = self.endpoint_traffic(entry.endpoint_id())?;
                 if entry.transfer_id().value >= ep_traf.end_index.len() {
-                    (Ongoing, transaction_count)
+                    (InterleavedOngoing, transaction_count)
                 } else {
-                    (Complete, transaction_count)
+                    let end = ep_traf.end_index.get(entry.transfer_id())?;
+                    (InterleavedComplete(end.value), transaction_count)
                 }
             },
             Some(Transaction(_, transaction_id)) => {
